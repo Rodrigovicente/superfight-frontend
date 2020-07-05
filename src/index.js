1e751e8cf4c45efca4ba1278b989db52371b03ab
Rodrigo Calabria <rodrigovcalabria@hotmail.com>
2020-07-05 10:21:52 -0300

    Setting server endpoint to openshift URL

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import socketIOClient from 'socket.io-client';
 //import 'bootstrap/scss/bootstrap.scss';
 import './index.scss';
 
-const ENDPOINT = process.env.REACT_APP_SERVER_URL;
+// const ENDPOINT = process.env.REACT_APP_SERVER_URL;
+const ENDPOINT = "http://superfight-backend-superfight-backend.apps.us-west-1.starter.openshift-online.com/";
 
 // class Card {
 // 	constructor(text, isChar, actions) {
