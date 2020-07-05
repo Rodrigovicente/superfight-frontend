568784faeef702564321c994972e443cb9832cf4
Rodrigo Calabria <rodrigovcalabria@hotmail.com>
2020-07-05 20:06:12 -0300

    Added build folder

---
+++ build/precache-manifest.b5e78a6a94b8c4212c98f49467ec8df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3461c9ff149f6bcb70a38608ee06ad66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cee18aae926c240dad84",
+    "url": "/static/css/main.ab7a59ee.chunk.css"
+  },
+  {
+    "revision": "ed12b269da7132c0d019",
+    "url": "/static/js/2.f67fdf06.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.f67fdf06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee18aae926c240dad84",
+    "url": "/static/js/main.a8a9d69b.chunk.js"
+  },
+  {
+    "revision": "415563a211a876526e65",
+    "url": "/static/js/runtime-main.f6e62b05.js"
+  }
+]);
